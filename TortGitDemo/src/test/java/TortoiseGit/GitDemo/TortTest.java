e4c51a23c5caef9a61046e8f8d6fc3e7eeaa5e64
veenaragaji <veenaragaji@gmail.com>
2020-07-16 18:33:25 +0530

    Commit2

--- TortGitDemo/src/test/java/TortoiseGit/GitDemo/TortTest.java
+++ TortGitDemo/src/test/java/TortoiseGit/GitDemo/TortTest.java
@@ -4,7 +4,7 @@ public class TortTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Tortoise Git demo");
+		System.out.println("Tortoise Git demo changed");
 		
 	}
 
